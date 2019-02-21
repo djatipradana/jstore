d7e496e8f3b38b425a5cffa8961b939a13dfb6b0
djatipradana <47686382+djatipradana@users.noreply.github.com>
2019-02-21 17:59:30 +0700

    Modul1

---
+++ DatabaseItem.java
@@ -0,0 +1,41 @@
+
+/**
+ * Write a description of class DatabaseItem here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class DatabaseItem
+{
+    // instance variables - replace the example below with your own
+    private String[] listItem;
+
+    /**
+     * Constructor for objects of class DatabaseItem
+     */
+    public void addItem()
+    {
+        // initialise instance variables
+        
+    }
+
+     public void remoteItem()
+    {
+        // initialise instance variables
+        
+    }
+    
+     public void getItemDatabase()
+    {
+        // initialise instance variables
+        
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    
+}
